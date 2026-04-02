7697d058ea486f12eb1eabaa8f7fb911f2031351
oDqnger <dimanchansilu@gmail.com>
2026-04-02 11:51:54 +0100

    got something working but only works on certain conditions. need to find a better algorithm for checking wheteher or a point is iwthin a triangle

--- main.cpp
+++ main.cpp
@@ -16,53 +16,39 @@
 using namespace std;
 using namespace glm;
 
-bool draw_line(float x, float y, float gradient, float intercept) {
-  if ((x*gradient + intercept) == y) {
-    return true;
-  }
-  return false;
-}
-
 int main() {
   uint8_t *data = (uint8_t*) malloc(sizeof(uint8_t) * WIDTH * HEIGHT * COLORCHANNEL);
   
   int index = 0;
   vector<vec2> vertices = {
-    // vec2(300, 300), vec2(500, 400),
-    // vec2(300, 300), vec2(500, 300),
-    vec2(500, 300), vec2(500, 400)
+    vec2(300, 300), vec2(500, 400), vec2(500, 300),
+    vec2(100, 100), vec2(200, 100), vec2(200, 200),
   };
-  
-  map<float, float> lines;
-  for (int x = 0; x<vertices.size(); x+=2) {
-    if ((vertices[x+1].x - vertices[x].x) != 0) {
-      float gradient = (vertices[x+1].y - vertices[x].y) / (vertices[x+1].x - vertices[x].x);
-      float intercept = vertices[x+1].y - (gradient * vertices[x+1].x);
-      lines[gradient] = intercept;
-    } else {
-      lines[0] = vertices[x].x;
-    }
+
+  vector<vector<float>> lines;
+  for (int i = 0; i<vertices.size(); i+=3) {
+    lines.push_back({vertices[i].y - vertices[i+1].y, vertices[i+1].x - vertices[i].x, vertices[i+1].x * vertices[i].y - vertices[i].x * vertices[i+1].y});
+    lines.push_back({vertices[i].y - vertices[i+2].y, vertices[i+2].x - vertices[i].x, vertices[i+2].x * vertices[i].y - vertices[i].x * vertices[i+2].y});
+    lines.push_back({vertices[i+1].y - vertices[i+2].y, vertices[i+2].x - vertices[i+1].x, vertices[i+2].x * vertices[i+1].y - vertices[i+1].x * vertices[i+2].y});
   }
-  bool done = false;
+  bool b = false;
   for (int y = 0; y<HEIGHT; y++) {
     for (int x = 0; x<WIDTH; x++) {
-      int i = 0;
-      for (auto const& [key, value]: lines) {
-        if (draw_line(x, y, key, value) && x>=std::min(vertices[i].x, vertices[i+1].x) && x<=std::max(vertices[i].x, vertices[i+1].x)) {
-          cout << x << endl;
+      for (int i = 0; i<lines.size(); i+=3) {
+        if ((lines[i][0]*x + lines[i][1]*y - lines[i][2]) <= 0 && (lines[i+1][0]*x + lines[i+1][1]*y - lines[i+1][2]) >= 0 && (lines[i+2][0]*x + lines[i+2][1]*y - lines[i+2][2]) <= 0) {
           data[index++] = 255;
           data[index++] = 255;
           data[index++] = 255;
-          done = true;
+          b = true;
+          break;
         }
-        i+=2;
       }
-      if (!done) {
+      if (!b) {
         data[index++] = 0;
         data[index++] = 0;
         data[index++] = 0;
       }
-      done = false;
+      b = false;
     }
   }
 
